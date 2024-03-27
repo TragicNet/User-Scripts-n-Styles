9d57070683068ddb5a3a797d78423c6e083bda84
TragicNet <67355216+TragicNet@users.noreply.github.com>
2024-03-28 05:03:19 +0530

    Update GrabRequiredModsLinks.js

--- NexusMods/GrabRequiredModsLinks.js
+++ NexusMods/GrabRequiredModsLinks.js
@@ -2,9 +2,9 @@
 // @name         Grab Mods Requiring Links
 // @namespace    http://tampermonkey.net/
 // @version      2024-03-27
-// @description  Adds a button in requirements which copies 'mods requiring this mod' links to clipboard, you can then keep them or open them using multiple url openers, your choice. I won't add that feature to make this lightweight.
+// @description  Adds a button in requirements which copies the links to clipboard, you can then keep them or open them using multiple url openers, your choice. I won't add that feature to make this lightweight.
 // @author       TragicNet
-// @include      /^https:\/\/www\.nexusmods\.com\/.*\/mods\/(?!$)\d+$/
+// @include      /^https:\/\/www\.nexusmods\.com\/.*\/mods\/\d+(\?.*)?$/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
 // @grant       GM_addStyle
 // ==/UserScript==
@@ -26,13 +26,27 @@ GM_addStyle(`
 `);
 
 window.addEventListener('load', (event) => {
-    let button = document.createElement('button');
-    button.setAttribute('id', 'grabLinkBtn');
-    button.innerHTML = 'Grab Links';
-    button.className = 'btn';
-    let reqBlock = document.querySelector('dd.clearfix:nth-child(2) > div:nth-child(2)');
-    button.addEventListener (
-        "click", () => grabLinks(reqBlock), false
-    );
-    reqBlock.insertBefore(button, reqBlock.firstChild);
+    let reqBlock1 = document.querySelector('dd.clearfix:nth-child(2) > div:nth-child(1)');
+    let reqBlock2 = document.querySelector('dd.clearfix:nth-child(2) > div:nth-child(2)');
+
+    if(reqBlock1.querySelector('.table')) {
+        let button1 = document.createElement('button');
+        button1.setAttribute('id', 'grabLinkBtn');
+        button1.innerHTML = 'Grab Links';
+        button1.className = 'btn';
+        button1.addEventListener (
+            "click", () => grabLinks(reqBlock1), false
+        );
+        reqBlock1.insertBefore(button1, reqBlock1.firstChild);
+    }
+    if(reqBlock2.querySelector('.table')) {
+        let button2 = document.createElement('button');
+        button2.setAttribute('id', 'grabLinkBtn');
+        button2.innerHTML = 'Grab Links';
+        button2.className = 'btn';
+        button2.addEventListener (
+            "click", () => grabLinks(reqBlock2), false
+        );
+        reqBlock2.insertBefore(button2, reqBlock2.firstChild);
+    }
 });
